e8ca2ab2d792feb02fec0a2ddc68e516766070c6
Andrei Bocan <zmaxor@gmail.com>
2011-09-27 18:38:08 +0300

    Noconflict commits

--- commits.rb
+++ commits.rb
@@ -3,13 +3,28 @@
 require 'open-uri'
 
 commits = ARGV[0].to_i
+noconflict = ARGV[1] == 'noconflict'
 
 
-commits.times do |index|
+def commit_noconflict(index)
+  filename = Time.now.to_f
+  `touch #{filename} && git add #{filename}`
+end
+
+def commit_conflict(index)
   File.open('content.txt', 'a') do |f|
     f.write("#{index}\n")
   end
-  `git add content.txt && git commit -m "#{open('http://whatthecommit.com/index.txt').read}"`
+  `git add content.txt`
+end
+
+commits.times do |index|
+  if noconflict 
+    commit_noconflict(index)
+  else
+    commit_conflict(index)
+  end
+  `git commit -m "#{open('http://whatthecommit.com/index.txt').read}"`
 end
 
 puts "Spawned #{commits} commits"
